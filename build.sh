ce5c2533f58493937bd2b3e8e7a3efdd1aa495f5
Se7enChat <se7enchat@zoho.com>
2014-02-06 22:08:06 -0800

    Update build scripts and Git ignore.
    Signed-off-by: Se7enChat <se7enchat@zoho.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Set file permissions correctly. The following directories must be writable.
-chmod 777 Public/Styles/
-chmod 777 Libraries/Database/SQLite/
+chmod 777 ./Public/Styles/
+chmod 777 ./Libraries/Database/SQLite/
 
 # Install the latest Composer because sometimes Travis' is out of date.
 curl -sS https://getcomposer.org/installer | php
